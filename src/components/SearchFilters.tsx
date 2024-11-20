538edfda880dbcf0534d06c3bac8f97a006ad328
ccuffari <75679158+ccuffari@users.noreply.github.com>
2024-11-20 11:36:08 +0100

    Commento a tasto di ricerca per inutilità

--- src/components/SearchFilters.tsx
+++ src/components/SearchFilters.tsx
@@ -87,16 +87,6 @@ export default function SearchFilters() {
         </div>
       </div>
 
-      {/* Pulsante di ricerca
-      <div className="mt-4">
-        <button
-          onClick={handleSearch}
-          className="w-full md:w-auto bg-blue-600 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-700 flex items-center justify-center"
-        >
-          <Search className="mr-2" />
-          {t("search.button")}
-        </button>
-      </div>
-    </div> */}
+    </div>
   );
 }
